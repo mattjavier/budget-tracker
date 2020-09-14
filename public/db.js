ce064efb3c2f0fb92a1a12ece4c7bb91d096bcc9
Matt Javier <matthewrjavier@gmail.com>
2020-09-13 21:55:43 -0700

    added service-worker connection, edit db, manifest

--- public/db.js
+++ public/db.js
@@ -39,6 +39,7 @@ const checkDatabase = () => {
         },
         body: JSON.stringify(getAll.result)
       })
+        .then(res => res.json())
         .then(() => {
           const transaction = db.transaction(['pending'], 'readwrite')
           const store = transaction.objectStore('pending')
